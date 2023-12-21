5e7ccf4b09b220ef9be568235518b290a032cdac
KenKomKom <kenichikomala@gmail.com>
2023-12-21 16:19:18 +0700

    50%

--- home/static/home.js
+++ home/static/home.js
@@ -16,6 +16,37 @@ function reveal(){
     };
 }
 
+function whiteNoise(){
+    var reveals = document.getElementById("trying-animate");
+
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals.getBoundingClientRect().top;
+    
+    var whenToShow = 150;
+    if(elementTop<windowHeight-whenToShow){
+        reveals.classList.add("animate");
+        document.getElementById("trying-padding").classList.add("reveal");
+    }else{
+        document.getElementById("trying-padding").classList.remove("reveal");
+        reveals.classList.remove("animate");
+    }
+}
+
+function revealHorBanner(){
+    var reveals = document.getElementById("first-animate");
+    
+    // console.log(element)
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals.getBoundingClientRect().top;
+    
+    var whenToShow = 10;
+    // console.log(reveals[i]+" "+windowHeight+" "+elementTop+" "+whenToShow)
+    if(elementTop<windowHeight-whenToShow){
+        reveals.classList.add("active");
+        // console.log(reveals[i].classList)
+    }
+}
+
 /*
 Parallax with js
 Do not question this code
@@ -45,5 +76,54 @@ function parallaxHeader(){
         }
     }
 }
+
+function cursorBlink(){
+    var cursor = document.getElementById("trying-cursor");
+    // console.log(cursor);
+    if(cursor.style.visibility == "visible"){
+        cursor.style.visibility = "hidden";
+    }else{
+        cursor.style.visibility="visible";
+    }
+}
+
+function ketik(){
+    var ketik = document.getElementById("trying-ketik");
+    // console.log(ketik.innerHTML);
+    if(ketik.innerHTML.length>=1 && del){
+        ketik.innerHTML=ketik.innerHTML.substring(0,ketik.innerHTML.length-1);
+    }else if(del){
+        del=false;
+        justFinishDel = true;
+    }
+    if(!del && justFinishDel){
+        var temp = words[Math.floor(Math.random() * words.length)];
+        while(temp==prior){
+            temp = words[Math.floor(Math.random() * words.length)];
+        }
+        prior=temp;
+        justFinishDel=false;
+    }else if(!del){
+        if(ketik.innerHTML!=prior){
+            ketik.innerHTML = prior.substring(0,ketik.innerHTML.length+1);
+        }else{
+            // console.log("selesai ketik");
+            del=true;
+            clearInterval(a);
+        }
+    }
+}
+
+var words = ["WebDev", "GameDev", "DataScience", "CP", "UI/UX"]
+var del = true;
+var justFinishDel = false;
+var prior = "WebDev"
+
+setInterval(cursorBlink, 650);
+setInterval(()=>{
+    a= setInterval(ketik, 90);
+}, 4000);
 window.addEventListener("scroll", reveal)
+window.addEventListener("scroll", whiteNoise)
+window.addEventListener("pageshow", revealHorBanner)
 window.addEventListener("scroll", parallaxHeader)
